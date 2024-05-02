7cde4bc9d2d0f24f72fa790f8ea12d7e77e9012f
Marble879 <younisakel879@gmail.com>
2024-05-02 17:27:15 +0200

    Improve docstrings

--- src/class_generator.py
+++ src/class_generator.py
@@ -1,7 +1,8 @@
 def get_class_json(file_names: list[str]) -> dict:
     """
     Takes a list of file names and returns a dict
-    representing the rdf classes
+    representing the rdf classes, where the file_names
+    represent the class names
     """
     rdf_classes = {"classes": []}
 
@@ -14,7 +15,8 @@ def get_class_json(file_names: list[str]) -> dict:
 def __create_dict(file_name: str) -> dict:
     """
     Takes the file name and returns a dict representing the
-    json representation of a class in rdf
+    json representation of a class in rdf, where the file_name
+    represents the class name
     """
     return {
         "ID": f"www.assignment3.com/{file_name}",
--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,9 @@ def dump_json(
     data_to_dump: dict, path: str = os.getcwd(), file_name: str = "json_dump"
 ) -> None:
     """
-    Takes as input a dict that is dumped to a json in the specified directory.
+    Takes as input a dict to be dumped to a json in the specified directory.
     If no directory is specified, it is dumped to the current directory.
+    If no filename is specified, it is defaulted to 'json_dump'
     """
     print(path)
     with open(f"{path}/{file_name}.json", "w", encoding="utf-8") as json_file:
