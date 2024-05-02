52cbb7311561a8d2770ab32201bce3668f763463
Chris Axt <81012809+ChrisAxt@users.noreply.github.com>
2024-05-02 09:53:01 +0200

    Updated files

--- fileImporter.py
+++ fileImporter.py
@@ -7,7 +7,7 @@ def importFiles ():
     csvs = glob('./Files/*.csv')
     for csv in csvs:
         name = csv[8:-4]
-        values = pd.read_csv(csv, sep="|")
+        values = pd.read_csv(csv, sep=",")
         dict[name] = values
 
     return dict
